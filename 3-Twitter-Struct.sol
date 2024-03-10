d5b678e008c50d8c3360059caee86a3e9c782bab
souhailbelmiloudi <souhailbelmiloudi@gmail.com>
2024-03-10 13:02:09 +0100

    struct and Array

--- 3-Twitter-Struct.sol
+++ 3-Twitter-Struct.sol
@@ -1,39 +1,31 @@
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
 
-pragma solidity ^0.8.24;
-
-// 1️⃣  Use require to limit the length of the tweet to be only 280 characters
-// HINT: use bytes to length of tweet
+// 1️⃣ Define a Tweet Struct with author, content, timestamp, likes
+// 2️⃣ Add the struct to array
+// 3️⃣ Test Tweets
 
 contract Twitter {
 
-    uint16 constant MAX_TWEET_LENGTH =280;
-
-    struct Tweet {
+    // define our struct
+    struct Tweet{
         address author;
         string content;
-        uint256 timestamp;
-        uint256 likes;
+        uint timestamp;
+        uint likes;
     }
 
     // add our code
     mapping(address => Tweet[] ) public tweets;
 
     function createTweet(string memory _tweet) public {
-        // conditional
-        // if tweet length <= 280 then we are good, otherwise we revert
-
-        // i have to convert the string into bytes so en ASCCI one charcacter takes up exactly one byts
-
-        require(bytes(_tweet).length <=MAX_TWEET_LENGTH, "Tweet is too long !");
-        
-        Tweet memory newTweet = Tweet({
-            author: msg.sender,
+        Tweet memory newTweet =Tweet({
+            author :msg.sender,
             content: _tweet,
-            timestamp: block.timestamp,
-            likes: 0
+            timestamp:block.timestamp,
+            likes :0
+                   
         });
-
         tweets[msg.sender].push(newTweet);
     }
 
@@ -41,8 +33,8 @@ contract Twitter {
         return tweets[msg.sender][_i];
     }
 
-    function getAllTweets(address _owner) public view returns (Tweet[] memory ){
-        return tweets[_owner];
+    function getAllTweets() public view returns (Tweet[] memory ){
+        return tweets[msg.sender];
     }
 
 }
